0b3cb910be3ae49132f57517d3058cc219a1cab8
ThiagoLuzFerreira <tlrferreira83@gmail.com>
2022-10-03 19:07:18 -0300

    perfil de teste criado

--- src/main/java/com/thiago/helpdesk/config/TestConfig.java
+++ src/main/java/com/thiago/helpdesk/config/TestConfig.java
@@ -1,4 +1,20 @@
 package com.thiago.helpdesk.config;
 
-public class Config {
+import com.thiago.helpdesk.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public void instanciaDB(){
+        dbService.instanciaDB();
+    }
 }
