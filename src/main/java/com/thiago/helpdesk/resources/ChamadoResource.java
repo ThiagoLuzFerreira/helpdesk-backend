da844d34ff534a187f1014d752418c313ba101df
ThiagoLuzFerreira <tlrferreira83@gmail.com>
2022-10-08 16:46:24 -0300

    findById para chamado

---
+++ src/main/java/com/thiago/helpdesk/resources/ChamadoResource.java
@@ -0,0 +1,25 @@
+package com.thiago.helpdesk.resources;
+
+import com.thiago.helpdesk.domain.Chamado;
+import com.thiago.helpdesk.domain.dtos.ChamadoDTO;
+import com.thiago.helpdesk.services.ChamadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+
+    @Autowired
+    private ChamadoService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
+        Chamado obj = service.findById(id);
+        return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+}
---
+++ src/main/java/com/thiago/helpdesk/services/ChamadoService.java
@@ -0,0 +1,29 @@
+package com.thiago.helpdesk.services;
+
+import com.thiago.helpdesk.domain.Chamado;
+import com.thiago.helpdesk.domain.Pessoa;
+import com.thiago.helpdesk.domain.Tecnico;
+import com.thiago.helpdesk.domain.dtos.TecnicoDTO;
+import com.thiago.helpdesk.repositories.ChamadoRepository;
+import com.thiago.helpdesk.repositories.PessoaRepository;
+import com.thiago.helpdesk.repositories.TecnicoRepository;
+import com.thiago.helpdesk.services.exceptions.DataIntegrityViolationException;
+import com.thiago.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id){
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. Id: " + id));
+    }
+}
