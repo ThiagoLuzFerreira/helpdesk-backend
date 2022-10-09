dabfa820412a07ac316be4b34f7668f0132d7615
ThiagoLuzFerreira <tlrferreira83@gmail.com>
2022-10-09 05:42:13 -0300

    endpoint create para Chamados implementado

--- src/main/java/com/thiago/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/thiago/helpdesk/resources/ChamadoResource.java
@@ -5,11 +5,11 @@ import com.thiago.helpdesk.domain.dtos.ChamadoDTO;
 import com.thiago.helpdesk.services.ChamadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,5 +31,12 @@ public class ChamadoResource {
         List<Chamado> list = service.findAll();
         List<ChamadoDTO> listDto = list.stream().map(x -> new ChamadoDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<Chamado> create(@Valid @RequestBody ChamadoDTO objDto){
+        Chamado obj = service.create(objDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/thiago/helpdesk/services/ChamadoService.java
+++ src/main/java/com/thiago/helpdesk/services/ChamadoService.java
@@ -1,10 +1,13 @@
 package com.thiago.helpdesk.services;
 
 import com.thiago.helpdesk.domain.Chamado;
+import com.thiago.helpdesk.domain.Cliente;
 import com.thiago.helpdesk.domain.Pessoa;
 import com.thiago.helpdesk.domain.Tecnico;
 import com.thiago.helpdesk.domain.dtos.ChamadoDTO;
 import com.thiago.helpdesk.domain.dtos.TecnicoDTO;
+import com.thiago.helpdesk.domain.enums.Prioridade;
+import com.thiago.helpdesk.domain.enums.Status;
 import com.thiago.helpdesk.repositories.ChamadoRepository;
 import com.thiago.helpdesk.repositories.PessoaRepository;
 import com.thiago.helpdesk.repositories.TecnicoRepository;
@@ -24,6 +27,10 @@ public class ChamadoService {
 
     @Autowired
     private ChamadoRepository repository;
+    @Autowired
+    private TecnicoService tecnicoService;
+    @Autowired
+    private ClienteService clienteService;
 
     public Chamado findById(Integer id){
         Optional<Chamado> obj = repository.findById(id);
@@ -32,5 +39,27 @@ public class ChamadoService {
 
     public List<Chamado> findAll(){
         return repository.findAll();
+    }
+
+    public Chamado create(@Valid ChamadoDTO objDto){
+        return repository.save(newChamado(objDto));
+    }
+
+    private Chamado newChamado(ChamadoDTO obj){
+        Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+        Cliente cliente = clienteService.findById(obj.getCliente());
+
+        Chamado chamado = new Chamado();
+        if(obj.getId() != null){
+            chamado.setId(obj.getId());
+        }
+
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+        chamado.setStatus(Status.toEnum(obj.getStatus()));
+        chamado.setTitulo(obj.getTitulo());
+        chamado.setObservacoes(obj.getObservacoes());
+        return chamado;
     }
 }
