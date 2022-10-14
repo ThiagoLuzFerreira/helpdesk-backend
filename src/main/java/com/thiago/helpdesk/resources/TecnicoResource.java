77a7101d110cfde3755d5520d6ed631af5e7d6e9
ThiagoLuzFerreira <tlrferreira83@gmail.com>
2022-10-14 00:04:16 -0300

    autorizacao com JWT

--- src/main/java/com/thiago/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/thiago/helpdesk/resources/TecnicoResource.java
@@ -5,6 +5,7 @@ import com.thiago.helpdesk.domain.dtos.TecnicoDTO;
 import com.thiago.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -33,6 +34,7 @@ public class TecnicoResource {
         return ResponseEntity.ok().body(listDto);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO objDto){
         Tecnico newObj = service.create(objDto);
@@ -40,12 +42,14 @@ public class TecnicoResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id, @Valid @RequestBody TecnicoDTO objDto){
         Tecnico obj = service.update(id, objDto);
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id){
         service.delete(id);
